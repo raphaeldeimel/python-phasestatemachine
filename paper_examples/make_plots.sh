c4d854f75d4f675e2f502ee32db06c49153520e7
Raphael Deimel <raphael@ist.sowasvon.org>
2017-09-04 11:55:54 +0200

    fix filenames for latex

--- paper_examples/make_plots.sh
+++ paper_examples/make_plots.sh
@@ -4,7 +4,8 @@ PYTHON=python3
 $PYTHON branch_and_aggregation.py  
 $PYTHON common_code.py  
 $PYTHON mdp_randomtransitions.py  
-$PYTHON slowing down and speeding up transitions.py  
 $PYTHON staying_and_leaving_state.py  
 $PYTHON terminal_states_and_exceptions.py
-
+$PYTHON enslaving_phase.py
+$PYTHON slowing_down_and_speeding_up_transitions.py
+$PYTHON branch_and_aggregation.py
