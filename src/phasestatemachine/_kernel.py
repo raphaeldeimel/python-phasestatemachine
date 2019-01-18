aa45c80fc1b8605e0b52453df1424195d96bcaf5
raphael.deimel <raphael.deimel@tu-berlin.de>
2019-01-18 10:45:35 +0100

    mask biases so they don't push the system beyond 1.0

--- src/phasestatemachine/_kernel.py
+++ src/phasestatemachine/_kernel.py
@@ -95,9 +95,9 @@ def _step(statevector,  #modified in-place
             correctiveActionSuccessor += correctiveAction[i,:]
         mu = correctiveActionPredecessor - correctiveActionSuccessor
 
-
-        biases = _np.dot(BiasMatrix, statevector)
-        velocity_offset = biases * dt 
+        statevector_abs = _np.abs(statevector)
+#        biases = _np.dot(BiasMatrix, statevector_abs) * (1-statevector_abs)
+        biases = _np.dot(BiasMatrix * _np.outer(1-statevector_abs,statevector_abs), statevector)
         noise_statevector = noise_velocity * dt
         
         #compute which transition biases should be applied right now:
