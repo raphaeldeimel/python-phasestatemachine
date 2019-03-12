48edd22b3b6acc0e59c23025bb95c8125c149a81
raphael.deimel <raphael.deimel@tu-berlin.de>
2019-03-12 11:38:39 +0100

    add script to plot effect of gamma parameter

--- paper_examples/make_plots.sh
+++ paper_examples/make_plots.sh
@@ -13,3 +13,4 @@ $PYTHON terminal_states_and_exceptions.py
 $PYTHON enslaving_phase.py
 $PYTHON slowing_down_and_speeding_up_transitions.py
 $PYTHON branch_and_aggregation.py
+$PYTHON gamma_parameter.py
