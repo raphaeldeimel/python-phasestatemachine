f6bbc64b344b7fcb3978b973e4d86ba264b9d98a
raphael.deimel <raphael.deimel@tu-berlin.de>
2020-05-12 16:03:04 +0200

    installation edge case fix

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ from setuptools import setup
 
 try:
     from catkin_pkg.python_setup import generate_distutils_setup
-    setup_args = generate_distutils_setup(
-        packages=['phasestatemachine'],
-        package_dir={'': 'src'})
-    setup(**setup_args)
-
 except ImportError:
     setup(name='Phase-state machine module',
           version='0.1.0',
           description='Library to create and execute phase-state machines',
           author='Raphael Deimel',
           author_email='raphael.deimel@tu-berlin.de',
-          url='http://www.mti-engage.tu-berlin.de/',
+          #url='http://www.mti-engage.tu-berlin.de/',
           packages=['phasestatemachine'],
           package_dir={'': 'src'},
          )
+else:
+    setup_args = generate_distutils_setup(
+        packages=['phasestatemachine'],
+        package_dir={'': 'src'})
+    setup(**setup_args)
 
